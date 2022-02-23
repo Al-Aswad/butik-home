3a0fc281d8dd75634ac26d37eccfa9b45a749793
Hajar Aswad <hajaraswadkom@gmail.com>
2022-02-23 11:42:32 +0800

    'test'

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}"
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif'],
+      'display': ['Oswald', 'sans-serif'],
+      'body': ['Inter', 'sans-serif'],
+    },
+    extend: {
+
+    }
+  },
+  plugins: [],
+}
